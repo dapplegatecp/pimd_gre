27951d3a244ab4ba1989a35a2499c7396dbf184b
Doug Applegate <dapplegate@cradlepoint.com>
2024-05-22 13:24:10 -0600

    tweak 2

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ RUN apk upgrade --no-cache && apk add --no-cache pimd iproute2
 COPY ./entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 
-CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug", "--disable-vifs"]
+# CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug", "--disable-vifs"]
+CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 sleep 20
 pim_rp=${PIM_RP:-192.168.127.1}
+
 pim_eth0=${PIM_ETH0:-eth0}
 pim_eth1=${PIM_ETH1:-eth1}
 
@@ -9,13 +10,15 @@ gre_key=${GRE_KEY:-0}
 gre_local=${GRE_LOCAL:-172.16.0.2/24}
 gre_remote=${GRE_REMOTE:-172.16.0.1}
 gre_gateway=${GRE_GATEWAY:-0.0.0.0}
+pim_gw=${PIM_GW:-${gre_remote}}
 
 # Create a GRE tunnel
 ip link add g0 type gre key $gre_key remote $gre_gateway ttl 64
 ip link set g0 multicast on
-ip addr add $gre_local peer $gre_remote dev g0
+ip addr add $gre_local dev g0
 ip link set g0 up
-ip route add $pim_rp/32 dev g0 via $gre_remote
+
+ip route add $pim_rp/32 via $pim_gw
 
 cat <<EOF > /etc/pimd.conf
 phyint g0 enable igmpv3
