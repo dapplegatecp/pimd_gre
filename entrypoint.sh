e808593031d56c44c7d883ba7e4c46e6e4f08d7d
Doug Applegate <dapplegate@cradlepoint.com>
2024-05-22 13:12:18 -0600

    tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -e
+sleep 20
+pim_rp=${PIM_RP:-192.168.127.1}
+pim_eth0=${PIM_ETH0:-eth0}
+pim_eth1=${PIM_ETH1:-eth1}
+
 gre_key=${GRE_KEY:-0}
 gre_local=${GRE_LOCAL:-172.16.0.2/24}
-gre_remote=${GRE_REMOTE:-172.16.0.1/24}
+gre_remote=${GRE_REMOTE:-172.16.0.1}
 gre_gateway=${GRE_GATEWAY:-0.0.0.0}
-pim_rp=${PIM_RP:-192.168.127.1}
 
 # Create a GRE tunnel
-ip link add g0 type gre key $gre_key remote $gre_gateway
+ip link add g0 type gre key $gre_key remote $gre_gateway ttl 64
 ip link set g0 multicast on
 ip addr add $gre_local peer $gre_remote dev g0
 ip link set g0 up
-ip route add $pim_rp/32 dev g0
+ip route add $pim_rp/32 dev g0 via $gre_remote
 
 cat <<EOF > /etc/pimd.conf
 phyint g0 enable igmpv3
-phyint eth0 enable igmpv3
+phyint $pim_eth0 enable igmpv3
+pyhint $pim_eth1 enable igmpv3
 rp-address $pim_rp 224.0.0.0/4
 EOF
 
