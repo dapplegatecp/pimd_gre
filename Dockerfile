47e3905fab5034337a95d9ca902bb506fff339d1
Doug Applegate <dapplegate@cradlepoint.com>
2024-06-04 18:06:22 -0600

    dmvpn

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine:edge
 RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-RUN apk upgrade --no-cache && apk add --no-cache pimd iproute2
+RUN apk upgrade --no-cache && apk add --no-cache pimd iproute2 opennhrp strongswan
+
+COPY ./opennhrp-script /etc/opennhrp/opennhrp-script
 
 COPY ./entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 
-# CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug", "--disable-vifs"]
-CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug"]
+CMD ["pimd", "-n", "-f", "/etc/pimd.conf", "-d", "all", "-l", "debug", "--disable-vifs"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,15 @@ sleep 20
 pim_rp=${PIM_RP:-192.168.127.1}
 
 pim_eth0=${PIM_ETH0:-eth0}
-pim_eth1=${PIM_ETH1:-eth1}
 
 gre_key=${GRE_KEY:-0}
 gre_local=${GRE_LOCAL:-172.16.0.2/24}
 gre_remote=${GRE_REMOTE:-172.16.0.1}
 gre_gateway=${GRE_GATEWAY:-0.0.0.0}
 pim_gw=${PIM_GW:-${gre_remote}}
+
+ipsec_key=${IPSEC_KEY:-1234}
+ipsec_local=${IPSEC_LOCAL:-75.0.0.1}
 
 # Create a GRE tunnel
 ip link add g0 type gre key $gre_key remote $gre_gateway ttl 64
@@ -23,8 +25,69 @@ ip route add $pim_rp/32 via $pim_gw
 cat <<EOF > /etc/pimd.conf
 phyint g0 enable igmpv3
 phyint $pim_eth0 enable igmpv3
-phyint $pim_eth1 enable igmpv3
 rp-address $pim_rp 224.0.0.0/4
 EOF
+
+cat <<EOF > /etc/opennhrp/opennhrp.conf
+interface g0
+  map $gre_remote/24 $gre_gateway register cisco
+  cisco-authentication 1234
+  holding-time 300
+  multicast nhs
+EOF
+
+cat <<EOF > /etc/swanctl/conf.d/dmvpn.conf
+connections {
+    DMVPN {
+        version = 2
+        rekey_time = 25920s
+        over_time = 2880s
+        local_addrs = $ipsec_local
+        remote_addrs = %any
+        proposals = aes256-sha384-ecp384
+        local {
+            auth = psk
+        }
+        local_port = 500
+        remote {
+            auth = psk
+        }
+        dpd_delay = 30
+        dpd_timeout = 105
+        children {
+            DMVPN {
+                mode = transport
+                remote_ts = dynamic[47/]
+                local_ts = dynamic[47/]
+                start_action = trap
+                close_action = trap
+                dpd_action = trap
+                esp_proposals = aes256gcm16-ecp384
+                rekey_time = 3240s
+                life_time = 3600s
+                sha256_96 = no
+            }
+        }
+        mobike = no
+        unique = replace
+    }
+}
+
+secrets {
+  ike {
+    id = $ipsec_local
+    secret = $ipsec_key
+  }
+}
+EOF
+
+ipsec start
+# wait for /var/run/charon.vivi
+while [ ! -e /var/run/charon.vici ]; do
+    sleep 1
+done
+swanctl --load-all
+
+opennhrp -c /etc/opennhrp/opennhrp.conf & 
 
 exec "$@"
